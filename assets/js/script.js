fa4195c643b75276d90c3313e3183c4a4dbb8ecf
DMariz <dmarizlocio@gmail.com>
2024-11-18 11:24:28 -0300

    Carousel working

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,20 @@ $(document).ready(function(){
     $('.slick-carousel').slick({
         autoplay: true,
         autoplaySpeed: 5000,
-        arrows: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev">Previous</button>',
+        nextArrow: '<button type="button" class="slick-next">Next</button>',
         dots: true,
         fade: true,
-        cssEase: 'linear'
+        cssEase: 'linear',
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false
+                }
+            }
+        ]
     });
 
     // Slick Carousel para Parceiros
